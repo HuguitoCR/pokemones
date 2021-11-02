33d61592fed3c57b3bba8a0c6bae05446c7ca500
Huguito CR <h.mejias.ugalde53@gmail.com>
2021-11-02 14:52:25 -0600

    commit inicial

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+
+const Pokemon = ({pokemon}) => {
+ const id = pokemon.url.split('/').filter(x => x).pop()
+  return (
+     <li>
+       <Link href={`/pokemones/${id}`} >{pokemon.name}</Link>
+       </li>
+ )
+
+}
+
+
+
+
+
+
+
+export default function Home({pokemones}) {
+  return (
+    <div>
+      <h1>Pokemones</h1>
+      <ul>
+        {pokemones.map(pokemon => (
+          <Pokemon key={pokemon.name} pokemon={pokemon} />
+        ))}
+       
+      </ul>
+
+    </div>
+   
+  )
+}
+
+
+export const getStaticProps = async () => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+  const data = await response.json();
+  
+  return {
+    props: { pokemones: data.results}
+  }
+}
